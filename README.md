4c984d3eaeca68511fd234ba8bd346028b7a7506
Cata <cata@cata.moe>
2024-09-17 17:09:35 +0200

    Changed services and moved all endpoints to waifu.pics

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 # NorthShirahebi
 ## A Medusa module for [NadekoBot](https://gitlab.com/kwoth/nadekobot)!
+Make your own https://nadekobot.readthedocs.io/en/latest/medusa/creating-a-medusa/
 
-### This Medusa pulls images from [waifu.pics](https://waifu.pics/) and [Nekos API](https://nekos.nekidev.com/)
+### This Medusa pulls images from [waifu.pics](https://waifu.pics/)
--- Shrine.cs
+++ Shrine.cs
@@ -1,148 +1,149 @@
+using System.Net.Http.Json;
 using Discord;
 using NadekoBot.Medusa;
-using NorthShirahebi;
+
+namespace NorthShirahebi;
 
 public sealed class Shrine : Snek
 {
-    public sealed class SocialInteractions : Snek
+    public new string Name = "Shrine";
+    private static readonly HttpClient Client = new HttpClient();
+    
+    [svc(Lifetime.Singleton)]
+    public sealed class ShrineService
     {
-        private readonly IImageFetcherService _service;
-        private static string UrlHug => "https://waifu.pics/api/sfw/hug";
-        private static string UrlPat => "https://waifu.pics/api/sfw/pat";
-        private static string UrlKiss => "https://waifu.pics/api/sfw/kiss";
-        private static string UrlWave => "https://waifu.pics/api/sfw/wave";
-        private static string UrlCuddle => "https://waifu.pics/api/sfw/cuddle";
-
-        public SocialInteractions(IImageFetcherService service)
+        public async Task<string> GetWaifuPicsImage(ImageType imageType)
         {
-            _service = service;
+            var img = await Client
+                .GetFromJsonAsync<WaifuData>($"https://waifu.pics/api/sfw/{imageType}")
+                .ConfigureAwait(false);
+        
+            return img.URL;
         }
-
-        [cmd]
-        public async Task Hug(AnyContext ctx, [inject] HttpClient httpClient, [leftover] string text = null)
-        {
-            await SendWaifuPicsEmbedAsync(ctx, httpClient, UrlHug, text);
-        }
-
-        [cmd]
-        public async Task Pat(AnyContext ctx, [inject] HttpClient httpClient, [leftover] string text = null)
-        {
-            await SendWaifuPicsEmbedAsync(ctx, httpClient, UrlPat, text);
-        }
-
-        [cmd]
-        public async Task Kiss(AnyContext ctx, [inject] HttpClient httpClient, [leftover] string text = null)
-        {
-            await SendWaifuPicsEmbedAsync(ctx, httpClient, UrlKiss, text);
-        }
-
-        [cmd]
-        public async Task Wave(AnyContext ctx, [inject] HttpClient httpClient, [leftover] string text = null)
-        {
-            await SendWaifuPicsEmbedAsync(ctx, httpClient, UrlWave, text);
-        }
-
-        [cmd]
-        public async Task Cuddle(AnyContext ctx, [inject] HttpClient httpClient, [leftover] string text = null)
-        {
-            await SendWaifuPicsEmbedAsync(ctx, httpClient, UrlCuddle, text);
-        }
-
-        private async Task SendWaifuPicsEmbedAsync(AnyContext ctx, [inject] HttpClient httpClient, string url,
-            string text)
+        
+        public async Task SendWaifuPicsEmbedAsync(AnyContext ctx, ImageType imageType, string text)
         {
             var emb = new EmbedBuilder();
 
             await ctx.Channel.TriggerTypingAsync().ConfigureAwait(false);
 
-            var img = await _service.GetWaifuPicsImage(url, httpClient);
+            var img = await GetWaifuPicsImage(imageType);
 
             if (!string.IsNullOrWhiteSpace(img))
             {
                 emb.WithImageUrl(img);
             }
 
-            if (!string.IsNullOrWhiteSpace(text))
+            switch(imageType)
             {
-                emb.WithDescription($"{ctx.User.Mention} cuddled {text}");
+                case ImageType.hug:
+                    emb.WithDescription($"{ctx.User.Mention} hugged {text}");
+                    break;
+                case ImageType.pat:
+                    emb.WithDescription($"{ctx.User.Mention} petted {text}");
+                    break;
+                case ImageType.kiss:
+                    emb.WithDescription($"{ctx.User.Mention} kissed {text}");
+                    break;
+                case ImageType.wave:
+                    emb.WithDescription($"{ctx.User.Mention} waved to {text}");
+                    break;
+                case ImageType.cuddle:
+                    emb.WithDescription($"{ctx.User.Mention} cuddled {text}");
+                    break;
+            }
+    
+            await ctx.Channel.EmbedAsync(emb);
+        }
+        
+        public async Task SendWaifuPicsEmbedAsync(AnyContext ctx, ImageType imageType)
+        {
+            var emb = new EmbedBuilder();
+
+            await ctx.Channel.TriggerTypingAsync().ConfigureAwait(false);
+
+            var img = await GetWaifuPicsImage(imageType);
+
+            if (!string.IsNullOrWhiteSpace(img))
+            {
+                emb.WithImageUrl(img);
             }
 
             await ctx.Channel.EmbedAsync(emb);
         }
     }
-
-    public sealed class Images : Snek
+    
+    public enum ImageType  
     {
-        private readonly IImageFetcherService _service;
-
-        public Images(IImageFetcherService service)
+        hug,
+        pat,
+        kiss,
+        wave,
+        cuddle,
+        waifu,
+        neko,
+        shinobu,
+        megumin
+    }
+    
+    public sealed class SocialInteractions(ShrineService service) : Snek
+    {
+        [cmd]
+        public async Task Hug(AnyContext ctx, [leftover] string text = null)
         {
-            _service = service;
+            await service.SendWaifuPicsEmbedAsync(ctx, ImageType.hug, text);
         }
 
         [cmd]
-        public async Task Catgirl(AnyContext ctx, [inject] HttpClient httpClient)
+        public async Task Pat(AnyContext ctx, [leftover] string text = null)
         {
-            await SendNekosEmbedAsync(ctx, httpClient, ImageFetcherService.Categories.Catgirl);
+            await service.SendWaifuPicsEmbedAsync(ctx, ImageType.pat, text);
         }
 
         [cmd]
-        public async Task Rain(AnyContext ctx, [inject] HttpClient httpClient)
+        public async Task Kiss(AnyContext ctx, [leftover] string text = null)
         {
-            await SendNekosEmbedAsync(ctx, httpClient, ImageFetcherService.Categories.Rain);
+            await service.SendWaifuPicsEmbedAsync(ctx, ImageType.kiss, text);
         }
 
         [cmd]
-        public async Task Weapon(AnyContext ctx, [inject] HttpClient httpClient)
+        public async Task Wave(AnyContext ctx, [leftover] string text = null)
         {
-            await SendNekosEmbedAsync(ctx, httpClient, ImageFetcherService.Categories.Weapon);
+            await service.SendWaifuPicsEmbedAsync(ctx, ImageType.wave, text);
         }
 
         [cmd]
-        public async Task Mountain(AnyContext ctx, [inject] HttpClient httpClient)
+        public async Task Cuddle(AnyContext ctx, [leftover] string text = null)
         {
-            await SendNekosEmbedAsync(ctx, httpClient, ImageFetcherService.Categories.Mountain);
+            await service.SendWaifuPicsEmbedAsync(ctx, ImageType.cuddle, text);
+        }
+    }
+
+    public sealed class Images(ShrineService service) : Snek
+    {
+        [cmd]
+        public async Task Waifu(AnyContext ctx)
+        {
+            await service.SendWaifuPicsEmbedAsync(ctx, ImageType.waifu);
         }
 
         [cmd]
-        public async Task Sportswear(AnyContext ctx, [inject] HttpClient httpClient)
+        public async Task Neko(AnyContext ctx)
         {
-            await SendNekosEmbedAsync(ctx, httpClient, ImageFetcherService.Categories.Sportswear);
+            await service.SendWaifuPicsEmbedAsync(ctx, ImageType.neko);
         }
 
         [cmd]
-        public async Task Baggyclothes(AnyContext ctx, [inject] HttpClient httpClient)
+        public async Task Shinobu(AnyContext ctx)
         {
-            await SendNekosEmbedAsync(ctx, httpClient, ImageFetcherService.Categories.BaggyClothes);
-        }
-
-        [cmd]
-        public async Task Dress(AnyContext ctx, [inject] HttpClient httpClient)
-        {
-            await SendNekosEmbedAsync(ctx, httpClient, ImageFetcherService.Categories.Dress);
-        }
-
-        [cmd]
-        public async Task Tree(AnyContext ctx, [inject] HttpClient httpClient)
-        {
-            await SendNekosEmbedAsync(ctx, httpClient, ImageFetcherService.Categories.Tree);
+            await service.SendWaifuPicsEmbedAsync(ctx, ImageType.shinobu);
         }
         
-        private async Task SendNekosEmbedAsync(AnyContext ctx, [inject] HttpClient httpClient, ImageFetcherService.Categories category)
+        [cmd]
+        public async Task Megumin(AnyContext ctx)
         {
-            var emb = new EmbedBuilder();
-
-            await ctx.Channel.TriggerTypingAsync().ConfigureAwait(false);
-
-            var img = await _service.GetRandomNekosImage((int)category, httpClient);
-
-
-            if (!string.IsNullOrWhiteSpace(img))
-            {
-                emb.WithImageUrl(img);
-            }
-
-            await ctx.Channel.EmbedAsync(emb);
+            await service.SendWaifuPicsEmbedAsync(ctx, ImageType.megumin);
         }
+
     }
 }
